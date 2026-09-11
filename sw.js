fd7b031de7ea4c083505c7d1c5ebec82af40df66
iamifatemi <f.amirhoseyn.131@gmail.com>
2026-09-11 18:51:58 +0330

    new build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const ZT_BUILD_VERSION = "1789135571";
+const ZT_BUILD_VERSION = "1789140091";
 const ZT_CACHE_NAME = "zt-webgl-" + ZT_BUILD_VERSION;
 
 self.addEventListener("install", function (event) {
